714bebafdd86c6ea1def2355dede23a8265d3323
JadeMawn <jade.mawn@richmond.edu>
2024-05-05 12:37:59 -0400

    Initial Commit

---
+++ nuevoSpanish/nuevoSpanishApp.swift
@@ -0,0 +1,17 @@
+//
+//  nuevoSpanishApp.swift
+//  nuevoSpanish
+//
+//  Created by Jade Mawn on 5/5/24.
+//
+
+import SwiftUI
+
+@main
+struct nuevoSpanishApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
